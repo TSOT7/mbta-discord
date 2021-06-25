16b813ca211c2adab62e65d0d7812d9b9d26ca66
Alex <alexywang03@gmail.com>
2021-06-25 15:16:19 -0400

    first draft

--- bot.py
+++ bot.py
@@ -1,17 +1,36 @@
 import discord
+from discord.ext import commands
+import requests
+import json
 
-client = discord.Client()
+description = "Basic bot used to connect to MBTA."
 
-@client.event
+bot = commands.Bot(command_prefix='mbta!', description=description)
+
+def getLinesFunc():
+    lines = requests.get("https://api-v3.mbta.com/routes")
+    linesJson = lines.json()
+    lineNames = []
+    for line in linesJson['data']:
+        lineNames.append(line.get("id"))
+    print(linesJson['data'])
+    return lineNames
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def update(ctx, line):
+    data = requests.get("https://api-v3.mbta.com/routes/" + line).json()
+    await ctx.send(data['data']['attributes']['direction_destinations'])
 
-    if message.content.startswith('!test'):
-        await message.channel.send('testing!')
+@bot.command()
+async def getlines(ctx):
+    await ctx.send(getLinesFunc())
 
-client.run('')
+
+bot.run('')
