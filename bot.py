365efcf7119c49d497391f9f9fa64abea3ba2878
Alex <alexywang03@gmail.com>
2021-06-25 16:08:46 -0400

    formatted update command

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ description = "Basic bot used to connect to MBTA."
 bot = commands.Bot(command_prefix='mbta!', description=description)
 
 def getLinesFunc():
-    lines = requests.get("https://api-v3.mbta.com/routes")
+    lines = requests.get("https://api-v3.mbta.com/lines/?include=routes")
     linesJson = lines.json()
     lineNames = []
     for line in linesJson['data']:
@@ -26,7 +26,12 @@ async def on_ready():
 @bot.command()
 async def update(ctx, line):
     data = requests.get("https://api-v3.mbta.com/routes/" + line).json()
-    await ctx.send(data['data']['attributes']['direction_destinations'])
+    termini = data['data']['attributes']['direction_destinations']
+    direction = data['data']['attributes']['direction_names']
+    zipped = list(zip(termini, direction))
+    print(zipped)
+    test = "Termini: " + zipped[0][0] + " ("+zipped[0][1]+ ")" + ", " + zipped[1][0] + " ("+zipped[1][1]+")"
+    await ctx.send(test)
 
 @bot.command()
 async def getlines(ctx):
